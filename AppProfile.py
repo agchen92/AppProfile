69b5cebc98657eedd2af5e548be9f784f5f0f595
Alan Chen <alanchen.agc@gmail.com>
2019-04-10 13:41:41 -0700

    Conducting Data Cleaning

--- AppProfile.py
+++ AppProfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[16]:
+# In[2]:
 
 
 '''
@@ -33,7 +33,7 @@ googleplay_header = googleplay[0]
 googleplay = googleplay[1:]
 
 
-# In[17]:
+# In[3]:
 
 
 #To make the data easier to explore. Let's create a function
@@ -51,7 +51,7 @@ def explore_data(dataset, start, end, rows_and_columns=False):
         print('Number of columns:', len(dataset[0]))
 
 
-# In[14]:
+# In[4]:
 
 
 #Explore App Store data.
@@ -63,7 +63,7 @@ explore_data(ios, 0, 3, True)
 #'price', 'rating_count_tot', 'rating_count_ver', 'user_rating', and 'prime_genre'.'
 
 
-# In[20]:
+# In[5]:
 
 
 #Explore Google Play data.
@@ -74,6 +74,89 @@ explore_data(googleplay, 0, 3, True)
 #From initial glance, we can see some relevant columns for this research
 #include "App", "Category", "Rating", "Type", "Price", "Content Rating",
 #and "Genres".
+
+
+# In[8]:
+
+
+#Now that we have explored some of the data inside this dataset
+#let's go ahead and perform some basic data cleaning. Per the discussion
+#on Kaggle, I have noticed that few of the rows have data that inconsistent
+#with the rest.
+
+print(googleplay[10472])  # incorrect row
+
+#The row 10472 matches the app called Life Made WI-Fi Touchscreen Photo Frame, 
+#and we can see that the rating is 19 above 5. I will delete this row because
+#the rating is an error.
+
+del googleplay[10472]
+
+
+# In[9]:
+
+
+#I will go ahead and perform more data cleaning on this dataset by
+#removing duplicate apps and those that are not English.
+
+for app in googleplay:
+    name = app[0]
+    if name == 'Instagram':
+        print(app)
+
+
+# In[10]:
+
+
+duplicate_apps = []
+unique_apps = []
+
+for app in googleplay:
+    name = app[0]
+    if name in unique_apps:
+        duplicate_apps.append(name)
+    else:
+        unique_apps.append(name)
+    
+print('Number of duplicate apps:', len(duplicate_apps))
+print('\n')
+print('Examples of duplicate apps:', duplicate_apps[:15])
+
+
+# In[12]:
+
+
+reviews_max = {}
+
+for app in googleplay:
+    name = app[0]
+    n_reviews = float(app[3])
+    
+    if name in reviews_max and reviews_max[name] < n_reviews:
+        reviews_max[name] = n_reviews
+        
+    elif name not in reviews_max:
+        reviews_max[name] = n_reviews
+        
+print('Expected length:', len(googleplay) - 1181)
+print('Actual length:', len(reviews_max))
+
+
+# In[15]:
+
+
+googleplay_clean = []
+already_added = []
+
+for app in googleplay:
+    name = app[0]
+    n_reviews = float(app[3])
+    
+    if (reviews_max[name] == n_reviews) and (name not in already_added):
+        googleplay_clean.append(app)
+        already_added.append(name)
+        
+explore_data(googleplay_clean, 0, 3, True)
 
 
 # In[ ]:
