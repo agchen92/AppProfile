c4911f50a4ee75b56adf589b8cdb8632ef0ec3c4
Alan Chen <alanchen.agc@gmail.com>
2019-04-09 18:01:30 -0700

    Preliminary Exploration

--- AppProfile.py
+++ AppProfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[16]:
 
 
 '''
@@ -29,6 +29,55 @@ ios = ios[1:]
 open_file = open('googleplaystore.csv')
 read_file = reader(open_file)
 googleplay = list(read_file)
-googleplay_header = ios[0]
-googleplay = ios[1:]
+googleplay_header = googleplay[0]
+googleplay = googleplay[1:]
+
+
+# In[17]:
+
+
+#To make the data easier to explore. Let's create a function
+#named explore_data so we can use it repeatedly to print rows
+#in a more readable way.
+
+def explore_data(dataset, start, end, rows_and_columns=False):
+    dataset_slice = dataset[start:end]    
+    for row in dataset_slice:
+        print(row)
+        print('\n') # adds a new (empty) line after each row
+
+    if rows_and_columns:
+        print('Number of rows:', len(dataset))
+        print('Number of columns:', len(dataset[0]))
+
+
+# In[14]:
+
+
+#Explore App Store data.
+print(ios_header)
+explore_data(ios, 0, 3, True)
+
+#We can see that the App Store data has 7197 rows and 16 colummns.
+#From initial glance, we can see that relevant columns include 'track_name'
+#'price', 'rating_count_tot', 'rating_count_ver', 'user_rating', and 'prime_genre'.'
+
+
+# In[20]:
+
+
+#Explore Google Play data.
+print(googleplay_header)
+explore_data(googleplay, 0, 3, True)
+
+#We can see that the Google Play data has 10841 rows and 13 colummns.
+#From initial glance, we can see some relevant columns for this research
+#include "App", "Category", "Rating", "Type", "Price", "Content Rating",
+#and "Genres".
+
+
+# In[ ]:
+
+
+
 
